233d7b738083ca5f3124165aacf2d2d80374830e
amankashyap004 <amankashyap004@gmail.com>
2022-11-06 20:37:08 +0530

    add some new features and fix error

--- main.js
+++ main.js
@@ -1,16 +1,16 @@
-// Reload Fuction
+// Reload Function
 function reloadPage() {
     location.reload();
 }
 
-// Random Number Fuction
+// Random Number Function
 function getRandomInt(max) {
     let r = Math.floor(Math.random() * max);
     r = Number.parseInt(r);
     return r;
 }
 
-// Input Fuction
+// Input Function
 function enterBtn() {
     let x = document.getElementById("input-number").value;
     document.getElementById("enter-number").innerHTML = x;
@@ -19,39 +19,40 @@ function enterBtn() {
             document.getElementById("invalid-number").innerText = " ";
     }
     else{
-        document.getElementById("invalid-number").innerText = "Please Enter A Vailid Number";
-        document.getElementById("winner").innerText = " "
-        document.getElementById("time-out").innerText = " "
+        document.getElementById("invalid-number").innerText = "Please Enter A Valid Number";
+        document.getElementById("winner").innerText = " ";
+        document.getElementById("time-out").innerText = " ";
+        guesses = 0;
     }
     return x;
 }
 
 let guesses = 0;
-let num = enterBtn()
+let num = enterBtn();
 document.getElementById("invalid-number").innerText = " ";
 
 // Play Function
 function play() {
     guesses++;
     let randomNum = getRandomInt(10);
-    document.getElementById("randam-number-is").innerHTML = randomNum;
-    console.log(num)
+    document.getElementById("random-number-is").innerHTML = randomNum;
+    // console.log(num)
     if (num == randomNum) {
-        document.getElementById("winner").innerText = "You Are a Winner"
-        document.getElementById("time-out").innerText = "Congratulations..!"
+        document.getElementById("winner").innerText = "You Are a Winner";
+        document.getElementById("time-out").innerText = "Congratulations..!";
         document.getElementById("guesses").innerText = guesses;
         if (num == randomNum) {
             guesses = 0;
         }
     }
     else if (num != randomNum) {
-        document.getElementById("winner").innerText = "Ohh..! You Missed"
-        document.getElementById("time-out").innerText = "Play Again"
-        num = enterBtn()
+        document.getElementById("winner").innerText = "Ohh..! You Missed";
+        document.getElementById("time-out").innerText = "Play Again";
+        num = enterBtn();
         // guesses++;
         document.getElementById("guesses").innerText = guesses;
     }
     else{
-        document.getElementById("invalid-number").innerText = "Pleas Enter A Vailid Number";
+        document.getElementById("invalid-number").innerText = "Pleas Enter A Valid Number";
     }
 }
